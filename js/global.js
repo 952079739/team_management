352fac525de5df94ddcddec493e5c5fb298abc2e
kongxin <1095716578@qq.com>
2019-10-10 20:53:09 +0800

    update

---
+++ js/global.js
@@ -0,0 +1,9 @@
+function showSetting() {
+    let oDiv = document.getElementsByClassName("setting")[0];
+    if (oDiv.style.display === "none"){
+        oDiv.style.display = "block";
+    }
+    else {
+        oDiv.style.display = "none";
+    }
+}
